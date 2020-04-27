85b9a44471131ee80a8dfd63790ba2c063ac5e01
sergicontre <sergicontre@users.noreply.github.com>
2020-04-27 11:39:46 +0200

    test: include testing for 100% coverage

--- test/pubsub.test.js
+++ test/pubsub.test.js
@@ -13,6 +13,25 @@ test('publish to channel a value', t => {
 	t.is(true, result);
 })
 
+test('publish to an existing channel', t => {
+	const pb = new PubSub();
+	let channel1 = pb.channels['channel-test'];
+	t.is(undefined, channel1);
+	pb.publish('channel-test',1);
+	channel1 = pb.channels['channel-test'];
+	t.not(undefined, channel1);
+	pb.publish('channel-test',4);
+	const channel2 = pb.channels['channel-test'];
+	t.is(true, channel1 === channel2);
+})
+
+test('publish to channel without name', t => {
+	const pb = new PubSub();
+	let result = pb.publish("", 2);
+	t.is(false,result)
+})
+
+
 test('subsbribe to channel first time', t => {
 	const pb = new PubSub();
 	let channel = pb.channels['channel-test'];
@@ -23,10 +42,18 @@ test('subsbribe to channel first time', t => {
 	t.is(22,pb.channels["channel-test"]._events[0]);
 })
 
-test('publish to channel without name', t => {
+test('subsbribe to an existing channel', t => {
 	const pb = new PubSub();
-	let result = pb.publish("", 2);
-	t.is(false,result)
+	let channel = pb.channels['channel-test'];
+	t.is(undefined, channel);
+	pb.subscribe("channel-test");
+	let channel1 = pb.channels['channel-test'];
+	t.not(undefined, channel1);
+
+	pb.subscribe('channel-test');
+	let channel2 = pb.channels['channel-test'];
+	t.not(undefined, channel2);
+	t.is(true, channel1 === channel2);
 })
 
 test('subsbribe to channel', t => {
